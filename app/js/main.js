ad29d0e3fdb9cc6837829d84be766f0f7ba3271f
Bogdan <bogdankozlovski18@gmail.com>
2021-05-27 10:42:11 +0300

    sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
 $(function () {
+  $(".filter-products__stars").rateYo({
+    starWidth: "11px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+  });
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: '$',
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
   $('.top__slider-inner').slick({
     arrows: false,
     dots: true,
@@ -6,10 +23,10 @@ $(function () {
   });
   var mixer = mixitup('.products__items');
   var mixer = mixitup('.new-design__list');
-
-
   $('.burger, .menu a').click(function () {
     $('.menu__list').toggleClass("active");
     $('.burger').toggleClass("active");
   });
+
+
 });
